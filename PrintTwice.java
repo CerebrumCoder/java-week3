24711c4f1919ae8d21daaa844d76f34b6c5a7daa
Neal Guarddin <neal@Neals-MacBook-Air.local>
2025-02-14 20:44:38 +0700

    Tambah PrintTwice.java

---
+++ PrintTwice.java
@@ -0,0 +1,20 @@
+
+public class PrintTwice {
+
+    public static void main(String[] args) {
+
+        System.out.print("Roman Alphabet: ");
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.print(c);
+        }
+        System.out.println();
+
+        
+        System.out.print("Greek alphabet: ");
+        for (int i = 913; i <= 937; i++) {
+            System.out.print((char) i);
+        }
+        System.out.println();
+
+    }
+}
